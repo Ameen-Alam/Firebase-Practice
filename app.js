dbaa22ce4870542bf1e96651ccb51944c0a686fb
ameen-alam <ameenalam202@gmail.com>
2022-07-23 11:54:41 +0500

    add local storage

--- app.js
+++ app.js
@@ -24,47 +24,48 @@ let register = () => {
         })
 }
 
-
 let login = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
 
     firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-        .then((res) => {
-            firebase.database().ref(`users/${res.user.uid}`).once('value', (data) => {
-                alert("successfully logged in")
-                window.location = "home.html"
-                console.log(data.val())
+    .then((res) => {
+        firebase.database().ref(`users/${res.user.uid}`).once('value', (data) => {
+            alert("successfully logged in")
+            let setData = JSON.stringify(data.val())
+            localStorage.setItem("data", setData)
+            window.location = "home.html"
+
             })
         })
         .catch((err) => {
             console.log('err=>', err)
         })
-
-}
-
-let logout = () => {
-    firebase.auth().signOut()
+        
+    }
+    
+    let logout = () => {
+        firebase.auth().signOut()
         .then(() => {
             alert('Signed Out');
+            localStorage.removeItem("data")
             window.location = "login.html"
         })
         .catch((err) => {
             console.log('err=>', err)
         })
-
-}
-
-
-// let abc = new Promise(() => {
-
-// })
-
-
-// abc
+        
+    }
+    
+    // let abc = new Promise(() => {
+        
+        // })
+        
+        
+        // abc
 //     .then(() => {
-
-//     })
-//     .catch(() => {
-
-//     })
+    
+    //     })
+    //     .catch(() => {
+        
+        //     })
